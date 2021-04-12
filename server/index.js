548f35727c0b801038435a8b57ada0c9c2774156
gparkkii <jyp933@gmail.com>
2021-04-12 18:25:30 +0900

    add :arrow_right: connect with mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect( config.mongoURI, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+).then(() => console.log("MongoDB connected..."))
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
