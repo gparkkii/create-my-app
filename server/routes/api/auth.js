ece8780b888bd063dd56ad8481ba821463b73c10
gparkkii <jyp933@gmail.com>
2021-04-12 19:02:12 +0900

    Add :arrow_right: Auth & Logout Route

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require("../middleware/auth");
 
 //=================================
 //             Auth
 //=================================
 
-router.get('/', auth , (req, res) => {''});
+router.get('/', auth , (req, res) => {
+  res.status(200).json({   
+    isAuth: true,
+    _id: req.user._id,
+    name: req.body.name,
+    email: req.user.email,
+    password: req.user.password,
+    profileImage: req.user.profileImage,
+    profileImage: req.user.profileImageType,
+    nickname: req.user.nickname,
+    description: req.body.userDescription,
+    url: req.user.url
+  })
+})
 
 module.exports = router;
--- server/routes/user/logout.js
+++ server/routes/user/logout.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require("../models/User");
+const { auth } = require("../middleware/auth");
 
 //=================================
 //             Logout
 //=================================
 
-router.post('/', (req, res) => {''});
+router.get('/', auth, (req, res) => {
+  console.log(req.body);
+  User.findOneAndUpdate(
+    { _id: req.user._id }, 
+    { token: "" },
+    (err, user) => {
+      if(err) return res.json({ success: false, err});
+      return res.status(200).send({ 
+        success: true,
+      })
+    }
+  )
+})
 
 module.exports = router;
