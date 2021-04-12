f9a9b364f574172ff50d5bd7663824552053a5bb
gparkkii <jyp933@gmail.com>
2021-04-12 18:57:33 +0900

    Add :arrow_right: bcrypt, JWT로 비밀번호 암호화 및 토큰생성

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -47,6 +50,49 @@ const userSchema = mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema);
+
+userSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  })
+}
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  user.token = token;
+  user.save(function(err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  })
+}
+
+userSchema.statics.findByToken = function(token, callback) {
+  var user = this;
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    user.findOne({ "_id" : decoded, "token" : token }, function(err, user) {
+      if (err) return callback(err);
+      callback(null, user);
+    })
+  })
+}
 
 module.exports = {
     User
