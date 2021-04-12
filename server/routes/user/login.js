ad57f7f2892e0c6be249b244327efe9a20fcaf59
gparkkii <jyp933@gmail.com>
2021-04-12 18:58:19 +0900

    Add :arrow_right: 로그인, 회원가입 라우트

--- server/routes/user/login.js
+++ server/routes/user/login.js
@@ -6,6 +6,28 @@ const { User } = require("../models/User");
 //             Login
 //=================================
 
-router.post('/', (req, res) => {''});
+router.post('/', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    console.log(req.body);
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    user.comparePassword(req.body.password , (err, isMatch ) => {
+      if (!isMatch)
+      return res.json({ success: false, message: "비밀번호가 틀렸습니다."})
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie("x_authExp", user.tokenExp);
+        res.cookie('x_auth', user.token)
+        .status(200)
+        .json({ success: true, userId: user._id })
+      })
+    })
+  })
+});
 
 module.exports = router;
--- server/routes/user/signup.js
+++ server/routes/user/signup.js
@@ -6,6 +6,20 @@ const { User } = require("../models/User");
 //             SignUp
 //=================================
 
-router.post('/', (req, res) => {''});
+router.post('/', (req, res) => {
+  const user = new User(req.body);
+  console.log(req.body);
+  user.save((err, user) => {
+    console.log(err);
+    if (err) return res.json({
+      success: false,
+      message: "회원가입에 실패하였습니다.",
+    })
+    return res.status(200).json({
+      success: true,
+      message: "회원가입에 성공하였습니다."
+    })
+  })
+});
 
 module.exports = router;
